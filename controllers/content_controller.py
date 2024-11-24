5943dac7621755c1f4a9d56133eeaf6ba9b24eba
MarinVLT <marilimatorres26@gmail.com>
2024-11-24 19:48:19 -0300

    Feat: Adicionando exportação de tabelas

--- controllers/content_controller.py
+++ controllers/content_controller.py
@@ -1,15 +1,18 @@
 # controllers/content_controller.py
-
 from dash.dependencies import Input, Output, State
-from dash import dcc
+from dash import dcc, html
 from dash.exceptions import PreventUpdate
 from services.docker_service import run_ck_analysis
 
+
 def register_callbacks(app):
     @app.callback(
-        [Output('output-container', 'children'),
-         Output('execute-button', 'children'),
-         Output('analysis-complete', 'data')],
+        [
+            Output('output-container', 'children'),
+            Output('execute-button', 'children'),
+            Output('analysis-complete', 'data'),
+            Output('export-tables-button', 'style')  # Adicionando novo Output
+        ],
         [Input('execute-button', 'n_clicks')],
         [State('github-url', 'value'),
          State('analysis-complete', 'data')]
@@ -17,11 +20,15 @@ def register_callbacks(app):
     def execute_analysis(n_clicks, url, analysis_complete):
         if n_clicks > 0:
             if not analysis_complete:
-                # Executa a análise CK
-                run_ck_analysis(url)  # Executa a análise e salva os dados nos arquivos CSV
-                
-                # Altera o botão para "Visualizar Resultados" e atualiza o estado de análise
-                return "", "Visualizar Resultados", True
+                run_ck_analysis(url)
+
+                # Retorna os valores necessários
+                return (
+                    html.Div(""),  # Conteúdo do container
+                    "Visualizar Resultados",
+                    True,
+                    {'display': 'inline-block'}
+                )
             else:
                 # Redireciona para a página de resultados
                 return dcc.Location(pathname='/results', id='redirect'), "Visualizar Resultados", True
